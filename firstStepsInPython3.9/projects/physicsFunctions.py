83dcb8a0908942778e85bbccb101bac5f01ccb97
EugeneAhramenko <akhramenko92@mail.ru>
2021-05-11 19:33:32 +0300

    physics Functions is a file that contains general physics functions

---
+++ firstStepsInPython3.9/projects/physicsFunctions.py
@@ -0,0 +1,50 @@
+"""
+Getting Ready for Physics Class
+"""
+
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+
+# This is function that convert Fahrenheit to Celsius
+def f_to_c(f_temp):
+    return (f_temp - 32) * 5 / 9
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
+
+
+# This is function that convert Celsius to Fahrenheit
+def c_to_f(c_temp):
+    return c_temp * (9 / 5) + 32
+
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+
+# Function for getting the force
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+
+# Function for getting energy
+def get_energy(mass, c=3 * 10 ** 8):
+    return mass * c ** 2
+
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
+
+
+# Function for getting work
+def get_work(mass, acceleration, distance):
+    # Getting the force using get_force
+    force = get_force(mass, acceleration)
+    return force * distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
